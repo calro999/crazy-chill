21d738eb23b3c7235fad4fc38cac100b29958d43
calro999 <gamingmedia.mail@gmail.com>
2026-07-20 19:07:54 +0900

    Fix Pinterest feed validation errors (ID/title limits, category depth, image URLs)

--- app/pinterest-feed.xml/route.ts
+++ app/pinterest-feed.xml/route.ts
@@ -19,25 +19,29 @@ export async function GET() {
         ? `【CRAZY CHILL】${nameJa} - ${product.tags.join(', ')}`
         : `【CRAZY CHILL】${nameJa} - 狂気的なまでに脱力するダークパンクアパレル`;
 
+      // Truncate for Pinterest limits
+      const safeId = product.id.substring(0, 127);
+      const safeTitle = nameJa.substring(0, 500);
+
       // 着用画像（Lookbook用画像）のマッピング
       let additionalImages = '';
       const lowerName = nameJa.toLowerCase();
       
       if (lowerName.includes('bone rider')) {
-        additionalImages += `\n      <g:additional_image_link>${baseUrl}/images/products/BONE%20RIDER.png</g:additional_image_link>`;
+        additionalImages += `\n      <g:additional_image_link><![CDATA[${baseUrl}/images/products/BONE%20RIDER.png]]></g:additional_image_link>`;
       }
       if (lowerName.includes('肋骨') || lowerName.includes('うさぎ')) {
-        additionalImages += `\n      <g:additional_image_link>${baseUrl}/images/products/%E5%85%A8%E8%BA%AB%E8%82%8B%E9%AA%A8%E3%81%A1%E3%82%83%E3%82%93.jpg</g:additional_image_link>`;
-        additionalImages += `\n      <g:additional_image_link>${baseUrl}/images/products/%E8%82%8B%E9%AA%A8%E3%81%A1%E3%82%83%E3%82%93.png</g:additional_image_link>`;
-        additionalImages += `\n      <g:additional_image_link>${baseUrl}/images/products/%E5%88%A5%E3%83%9D%E3%83%BC%E3%82%BA2%E5%85%A8%E8%BA%AB%E8%82%8B%E9%AA%A8.jpg</g:additional_image_link>`;
+        additionalImages += `\n      <g:additional_image_link><![CDATA[${baseUrl}/images/products/%E5%85%A8%E8%BA%AB%E8%82%8B%E9%AA%A8%E3%81%A1%E3%82%83%E3%82%93.jpg]]></g:additional_image_link>`;
+        additionalImages += `\n      <g:additional_image_link><![CDATA[${baseUrl}/images/products/%E8%82%8B%E9%AA%A8%E3%81%A1%E3%82%83%E3%82%93.png]]></g:additional_image_link>`;
+        additionalImages += `\n      <g:additional_image_link><![CDATA[${baseUrl}/images/products/%E5%88%A5%E3%83%9D%E3%83%BC%E3%82%BA2%E5%85%A8%E8%BA%AB%E8%82%8B%E9%AA%A8.jpg]]></g:additional_image_link>`;
       }
 
       const imageUrl = product.image.startsWith('http') ? product.image : `${baseUrl}${product.image}`;
 
       return `
     <item>
-      <g:id>${product.id}</g:id>
-      <g:title><![CDATA[${nameJa}]]></g:title>
+      <g:id>${safeId}</g:id>
+      <g:title><![CDATA[${safeTitle}]]></g:title>
       <g:description><![CDATA[${description}]]></g:description>
       <g:link>${baseUrl}/ja/products/${product.id}</g:link>
       <g:image_link><![CDATA[${imageUrl}]]></g:image_link>${additionalImages}
@@ -46,7 +50,7 @@ export async function GET() {
       <g:condition>new</g:condition>
       <g:brand>CRAZY CHILL</g:brand>
       <g:product_type><![CDATA[${product.category}]]></g:product_type>
-      <g:google_product_category>166</g:google_product_category>
+      <g:google_product_category><![CDATA[Apparel & Accessories > Clothing]]></g:google_product_category>
     </item>`;
     }).join('')}
   </channel>
